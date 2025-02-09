82390427fd0d7fad00c6f0688970f927fbe7687f
Raman7913 <raman7913523@gmail.com>
2025-02-09 22:10:49 +0545

    added loading screen animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
-function App() {
-    return <></>;
-}
+import React, { useState } from "react";
+import LoadingScreen from "./components/LoadingScreen";
+
+const App = () => {
+    const [loading, setLoading] = useState(true);
+    return (
+        <>{loading && <LoadingScreen onComplete={() => setLoading(false)} />}</>
+    );
+};
 
 export default App;
---
+++ src/components/LoadingScreen.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+
+const LoadingScreen = ({ onComplete }) => {
+    const [text, setText] = useState("");
+    const fullText = "Welcome to my site";
+
+    useEffect(() => {
+        let index = 0;
+
+        const loadingFucn = () => {
+            if (index < fullText.length) {
+                const currentLetter = fullText[index];
+                setText((prevText) => {
+                    return prevText + currentLetter;
+                });
+                index++;
+            } else {
+                setTimeout(() => {
+                    onComplete();
+                }, 500);
+            }
+        };
+        const interval = setInterval(loadingFucn, 100);
+        return () => clearInterval(interval);
+    }, []);
+    return (
+        <div className="fixed inset-0 z-50 bg-black text-gray-100 flex flex-col justify-center items-center">
+            <div className="mb-4 text-4xl font-mono font-bold">
+                {text}
+                <span className="animate-blink ml-1"></span>
+            </div>
+            <div className="w-[200px] h-[2px] bg-gray-800 rounded relative overflow-hidden">
+                <div className="w-[40%] h-full bg-blue-500 shadow-[0_0_15px_#3b82f6] animate-loading-bar"></div>
+            </div>
+        </div>
+    );
+};
+
+export default LoadingScreen;
